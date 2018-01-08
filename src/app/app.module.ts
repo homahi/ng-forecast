cdee2d79e683aff6821b93dbeac82cbfedd0fd48
homahi <homahi0128@gmail.com>
2018-01-08 09:45:24 +0900

    9-2-1:app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimtionsModule } from '@angular/platfrom-browser/animations';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import {
+  MatToolbarModule,
+  MatProgressBarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatCardModule,
+  MatInputModule
+} from '@angular/material';
 
 
 @NgModule({
@@ -11,6 +23,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BrowserAnimtionsModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    HttpModule,
     BrowserModule,
     AppRoutingModule
   ],
