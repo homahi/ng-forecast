ff2cb995d0a6d3886382785dbe6aae9fdd3af953
homahi <homahi0128@gmail.com>
2018-01-08 14:01:46 +0900

    9-5-3:Pipeでja-JPが使えない

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { OpenWeatherMapService } from './services/open-weather-map.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -20,6 +20,7 @@ import {
 import { HomeComponent } from './home/home.component';
 import { AreaEditComponent } from './area-edit/area-edit.component';
 import { ForecastComponent } from './forecast/forecast.component';
+import { UnixTimeDatePipe } from './pipes/unix-time-date.pipe';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { ForecastComponent } from './forecast/forecast.component';
     AppComponent,
     HomeComponent,
     AreaEditComponent,
-    ForecastComponent
+    ForecastComponent,
+    UnixTimeDatePipe
   ],
   imports: [
     BrowserAnimationsModule,
@@ -43,7 +45,7 @@ import { ForecastComponent } from './forecast/forecast.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [OpenWeatherMapService],
+  providers: [{provide: LOCALE_ID, useValue: 'ja-JP'},OpenWeatherMapService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
