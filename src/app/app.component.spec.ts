f86635db49ac9e8fdaaef31d217ee9643bdd0a14
homahi <homahi0128@gmail.com>
2018-01-09 19:57:00 +0900

    10-5-2:テストの練習

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixtureAutoDetect, ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let el: HTMLElement;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -10,22 +15,31 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true }
+      ]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    el = fixture.debugElement.query(By.css('h1')).nativeElement;
+  });
+
+  it('タイトルが表示されるべき', () => {
+    expect(el.textContent).toContain(component.title);
+  });
+
+  it('comp.titleを変更しても元のタイトルが表示されるべき', () => {
+    const oldTitle = component.title;
+    component.title = 'Test Title';
+    expect(el.textContent).toContain(oldTitle);
+  });
+
+  it('detectChangesを呼べばタイトルは変更されているべき', () => {
+    component.title = 'Test Title';
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+    expect(el.textContent).toContain(component.title);
+  });
 });
